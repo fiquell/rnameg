3d9e99a843f47b79426bbcf89125bbf5ae458b21
Fiquell <fiquellh@gmail.com>
2023-09-04 05:42:53 +0800

    Updated descriptions and help information for this utility

--- src/options.rs
+++ src/options.rs
@@ -35,21 +35,31 @@ impl Options {
         {
             let mut parser = ArgumentParser::new();
 
-            parser.set_description("Desc");
+            parser.set_description(
+                "A command-line utility for renaming files or directories with random strings",
+            );
             parser
                 .refer(&mut args)
-                .add_argument("**/*", List, "Args")
+                .add_argument(
+                    "SOURCES",
+                    List,
+                    "One or more file or directory paths to process",
+                )
                 .required();
-            parser
-                .refer(&mut length)
-                .add_option(&["-l", "--length"], Store, "Length");
-            parser
-                .refer(&mut recursive)
-                .add_option(&["-r", "--recursive"], StoreTrue, "Recursive");
+            parser.refer(&mut length).add_option(
+                &["-l", "--length"],
+                Store,
+                "Set string length randomly (default 20)",
+            );
+            parser.refer(&mut recursive).add_option(
+                &["-r", "--recursive"],
+                StoreTrue,
+                "Enable recursive directory processing",
+            );
             parser.add_option(
                 &["-v", "--version"],
                 Print(env!("CARGO_PKG_VERSION").to_string()),
-                "Version",
+                "Show this version information message and exit",
             );
             parser.parse_args_or_exit();
         }
